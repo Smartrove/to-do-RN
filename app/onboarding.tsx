936a28dfc0647b863be8096c8e79034159da530f
smartrove <olaokunolalekan@gmail.com>
2026-01-13 17:31:25 +0100

    rn

---
+++ app/onboarding.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'expo-router'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const onboarding = () => {
+  return (
+    <View>
+      <Text>onboarding</Text>
+      
+    </View>
+  )
+}
+
+export default onboarding
+
+const styles = StyleSheet.create({})

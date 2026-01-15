9ea1325e56d8e44c09bdabac13eb64f1d48fb648
smartrove <olaokunolalekan@gmail.com>
2026-01-15 01:10:33 +0100

    implemented search

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import SearchBar from "@/components/serachbar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 
 export default function HomeScreen() {
+  const router = useRouter();
   return (
     <View className="flex-1 bg-primary">
       <Image
@@ -18,7 +20,10 @@ export default function HomeScreen() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
         <View className="flex-1 mt-5">
-          <SearchBar />
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
         </View>
       </ScrollView>
     </View>
